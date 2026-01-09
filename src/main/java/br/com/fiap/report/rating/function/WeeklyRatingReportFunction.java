044e171b2a566c56a0bdcae95cd38310fbdc92d5
webberchagas <webberschagas@gmail.com>
2026-01-08 22:25:07 -0300

    .

--- src/main/java/br/com/fiap/report/rating/function/WeeklyRatingReportFunction.java
+++ src/main/java/br/com/fiap/report/rating/function/WeeklyRatingReportFunction.java
@@ -20,7 +20,7 @@ public class WeeklyRatingReportFunction {
     public void run(
         @TimerTrigger(
                 name = "WeeklyRatingReportTrigger",
-                schedule = "59 59 23 * * 7"
+                schedule = "0 */5 * * * *"
         ) String timerInfo,
         final ExecutionContext executionContext,
         @ServiceBusQueueOutput(
